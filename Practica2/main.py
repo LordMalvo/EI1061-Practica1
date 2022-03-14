efb2719dedb960618853e4a0f9e82d22c63cca7c
VictorinoPlatino <al375889@uji.es>
2022-03-14 18:42:12 +0100

    Primeros cambios, nos hemos quedado en la etapa COMMIT. Falta actualizar las estructuras una vez hechos los cambios en el registro específico.

--- Practica2/main.py
+++ Practica2/main.py
@@ -84,6 +84,8 @@ def iniciarEstructuras(instrucciones):
     REG = []
     DAT = []
     INS = []
+    ROB = []
+
     num = 1
 
     for i in range(16):
@@ -91,6 +93,11 @@ def iniciarEstructuras(instrucciones):
 
     for i in range(32):
         DAT.append(i)
+
+
+
+    for i in range(len(instrucciones)):
+        ROB.append(LineaROB(i,0,"","",0,0,""))
 
     for inst in instrucciones:
         op = inst[0]
@@ -106,12 +113,73 @@ def iniciarEstructuras(instrucciones):
             rs = inst[1][2]
             INS.append(Instruccion(op, "", rs, rt, inm, num))
             num += 1
-    return REG, DAT, INS
+
+    return REG, DAT, INS, ROB
+
+
+def iniciarER(instrucciones):
+    ER = [[],[],[]] # [0] es para ALU, [1] para MEM, [2] para MULT
+    #busy, op, opa, opa_ok, ciclo_ok_opa, opb, opb_ok, ciclo_ok_opb, inm, rob
+    for ins in instrucciones:
+        if ins[0] == "add" or ins[0] == "sub" :
+            ER[0].append(LineaER(0,"","",0,0,"",0,0,"",0))
+
+        elif ins[0] == "mult":
+            ER[1].append(LineaER(0, "", "", 0, 0, "", 0, 0, "", 0))
+
+        else:
+            ER[2].append(LineaER(0, "", "", 0, 0, "", 0, 0, "", 0))
+
+    return ER
+
+
+
+
+def etapa_COMMIT( ):
+    # Saber que linea hay que eliminar
+    linea_rob = ROB[linea]
+
+
+    if linea_rob.busy == 1 and linea_rob.etapa == "WB" and linea_rob.valor_ok == 1 and linea_rob.ciclo_ok:
+        id_reg = linea_rob.rd
+
+        if (linea_rob.rob == id_reg.rob):
+              #actualizar contenido id_reg.contenido
+
+
 
 if __name__ == '__main__':
+
     # Inicializamos estructuras (Registros, memoria de datos y memoria de isntrucciones)
     instrucciones = read_data()
-    REG, DAT, INS = iniciarEstructuras(instrucciones)
+    REG, DAT, INS, ROB = iniciarEstructuras(instrucciones)
+    ER = iniciarER(INS)
+    ciclo = 1
+
+    inst_prog = len(instrucciones)
+    inst_rob = 0
+
+    while (inst_rob > 0) or (inst_prog > 0):
+        etapa_ID_ISS()
+        etapa_EX()
+        etapa_WD()
+        etapa_COMMIT()
+
+        ciclos = cliclos+1
+
+        mostrar_ER(ER)
+        mostrar_ROB(ROB)
+        mostrar_REG(REG)
+
+
+
+
+
+
+
+
+
+
 
 
 
